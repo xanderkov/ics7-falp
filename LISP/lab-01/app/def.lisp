6a84250ecb98180755d2c358146c9e3681bc75e2
xanderkov <xanderkovel@gmail.com>
2023-03-20 11:13:41 +0300

    Я ненавижу ЛИСП, я ненавижу ЛИСП

---
+++ LISP/lab-01/app/def.lisp
@@ -0,0 +1,18 @@
+
+(print 
+(cons 1 
+    (cons (cons 2 
+            (cons 3 
+                (cons (cons 4 (cons Nil Nil)) 
+                    (cons 5 Nil)))) 
+        (cons 6 (cons (cons 7 (cons 8 (cons 9 Nil))) 
+            (cons 'a 'b))))))(print 
+    (cons 2 
+    (cons 3
+        (cons (cons 4 (cons Nil Nil))
+            (cons 5 Nil)))))
+
+(print (cons 6 (cons
+(cons 7 
+        (cons 8
+            (cons 9 Nil))) (cons ('a 'b)))))
---
+++ LISP/lab-01/app/try.lisp
@@ -0,0 +1,12 @@
+(
+                cons 3 (
+                    cons (
+                        cons 4 (
+                            cons (
+                                nil nil
+                            )
+                        )
+                        cons (5 nil)
+                    )
+                )
+            )
